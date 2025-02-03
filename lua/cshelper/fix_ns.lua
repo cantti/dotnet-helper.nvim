d8a0129611515af40358a98101b2aa925ed23e0c
cantti <cantti@outlook.com>
2025-02-03 02:51:29 +0100

    feat: use ui.input

--- lua/cshelper/fix_ns.lua
+++ lua/cshelper/fix_ns.lua
@@ -21,46 +21,47 @@ local function write_to_file(path, content)
 end
 
 function M.execute()
-  local dir = vim.fn.input("Enter directory path: ", ".", "dir")
-  local files = utils.get_file_options(dir, { "cs" })
-  local updated_files = {}
-  local transitions = {}
+  vim.ui.input({ prompt = "Enter directory: ", default = ".", completion = "dir" }, function(dir)
+    local files = utils.get_file_options(dir, { "cs" })
+    local updated_files = {}
+    local transitions = {}
 
-  -- update namespace in files
-  for _, file_path in ipairs(files) do
-    local content = readAll(file_path)
-    local old_ns = string.match(content, "namespace%s+([^;%s]+)")
-    local new_ns = utils.get_namespace_for_file(file_path)
+    -- update namespace in files
+    for _, file_path in ipairs(files) do
+      local content = readAll(file_path)
+      local old_ns = string.match(content, "namespace%s+([^;%s]+)")
+      local new_ns = utils.get_namespace_for_file(file_path)
 
-    if old_ns and old_ns ~= new_ns then
-      -- namespace can end on ; or space (new line)
-      content = string.gsub(content, "namespace%s+[^;%s]+", "namespace " .. new_ns)
-      table.insert(updated_files, file_path)
-      transitions[old_ns] = new_ns
-      write_to_file(file_path, content)
+      if old_ns and old_ns ~= new_ns then
+        -- namespace can end on ; or space (new line)
+        content = string.gsub(content, "namespace%s+[^;%s]+", "namespace " .. new_ns)
+        table.insert(updated_files, file_path)
+        transitions[old_ns] = new_ns
+        write_to_file(file_path, content)
+      end
     end
-  end
 
-  vim.print(transitions)
+    vim.print(transitions)
 
-  -- update usings
-  local all_files = utils.get_file_options(".", { "cs" })
-  for _, file_path in ipairs(all_files) do
-    local content = readAll(file_path)
-    local is_updated = false
-    for old_ns, new_ns in pairs(transitions) do
-      local count
-      content, count = string.gsub(content, "using%s+" .. old_ns .. ";", "using " .. new_ns .. ";")
-      is_updated = is_updated or count > 0
+    -- update usings
+    local all_files = utils.get_file_options(".", { "cs" })
+    for _, file_path in ipairs(all_files) do
+      local content = readAll(file_path)
+      local is_updated = false
+      for old_ns, new_ns in pairs(transitions) do
+        local count
+        content, count = string.gsub(content, "using%s+" .. old_ns .. ";", "using " .. new_ns .. ";")
+        is_updated = is_updated or count > 0
+      end
+      if is_updated then
+        write_to_file(file_path, content)
+      end
     end
-    if is_updated then
-      write_to_file(file_path, content)
-    end
-  end
-  -- update buffers
-  vim.cmd("bufdo checktime")
+    -- update buffers
+    vim.cmd("bufdo checktime")
 
-  vim.print("Updated files count: " .. vim.tbl_count(updated_files))
+    vim.print("Updated files count: " .. vim.tbl_count(updated_files))
+  end)
 end
 
 return M
