89ee708523cd0bc74ad6bed71e3a0c6cc8442dc5
cantti <cantti@outlook.com>
2025-08-25 23:06:19 +0200

    improve nuget search

--- lua/cshelper/nuget.lua
+++ lua/cshelper/nuget.lua
@@ -3,11 +3,21 @@ local fs = require("cshelper.fs")
 
 local M = {}
 
-local function on_select(package)
-  local targets = utils.get_projects(false)
-  if vim.tbl_count(targets) == 1 then
+local function on_select_project(project, package)
+  local args = { "dotnet", "add", project, "package", package.id }
+  local output = vim.system(args):wait()
+  if output.code == 0 then
+    vim.cmd("edit " .. project)
+    vim.notify("Package added", vim.log.levels.INFO)
+  end
+end
+
+local function on_select_search_result(package)
+  local projects = utils.get_projects(false)
+  if vim.tbl_count(projects) == 1 then
+    on_select_project(projects[1], package)
   else
-    vim.ui.select(targets, {
+    vim.ui.select(projects, {
       prompt = "Choose project:",
       format_item = function(item)
         return fs.relative_path(item)
@@ -16,12 +26,7 @@ local function on_select(package)
       if not project then
         return
       end
-      local args = { "dotnet", "add", project, "package", package.id }
-      dd(args)
-      local output = vim.system(args):wait()
-      if output.code == 0 then
-        vim.notify("Package added", vim.log.levels.INFO)
-      end
+      on_select_project(project, package)
     end)
   end
 end
@@ -29,21 +34,32 @@ end
 local function search(input)
   local output = vim.system({ "dotnet", "package", "search", input, "--format", "json" }):wait()
   local result = vim.json.decode(output.stdout)
-  local packages = result.searchResult[1].packages
+  local packages = {}
+  for _, searchResult in ipairs(result.searchResult) do
+    for _, package in ipairs(searchResult.packages) do
+      table.insert(packages, {
+        id = package.id,
+        version = package.latestVersion,
+        downloads = package.totalDownloads,
+        source = searchResult.sourceName,
+      })
+    end
+  end
   local max_id = 0
   for _, item in ipairs(packages) do
     max_id = math.max(max_id, #item.id)
   end
-  vim.ui.select(result.searchResult[1].packages, {
+  vim.ui.select(packages, {
     format_item = function(item)
       return string.format(
-        "%-" .. max_id .. "s | Version: %-12s | Downloads: %-10s",
+        "%-" .. max_id .. "s | Version: %-12s | Downloads: %-10s | Source: %s",
         item.id,
-        item.latestVersion,
-        item.totalDownloads
+        item.version,
+        item.downloads,
+        item.source
       )
     end,
-  }, on_select)
+  }, on_select_search_result)
 end
 
 function M.search()
