3101d80ff6ca914c2bcf16ac6b4b5236455d52c2
cantti <cantti@outlook.com>
2025-08-26 17:20:40 +0200

    nuget improvements

--- lua/cshelper/nuget.lua
+++ lua/cshelper/nuget.lua
@@ -1,41 +1,63 @@
 local utils = require("cshelper.utils")
 local fs = require("cshelper.fs")
 
+---@class NugetPackage
+---@field id string
+---@field version string
+---@field downloads integer
+---@field source string
+---@field description string
+
+---@class Nuget
+---@field packages NugetPackage[]|nil
+---@field selected_package NugetPackage|nil
+---@field selected_project string|nil
+---@field selected_version string|nil
 local Nuget = {}
+
 Nuget.__index = Nuget
 
 function Nuget:new()
   local obj = setmetatable({}, Nuget)
   obj.packages = nil
-  obj.package = nil
-  obj.project = nil
-  obj.version = nil
+  obj.selected_package = nil
+  obj.selected_project = nil
+  obj.selected_version = nil
   return obj
 end
 
-function Nuget:_prompt_package()
+---@package
+function Nuget:prompt_package()
   vim.ui.select(self.packages, {
+    ---@param item NugetPackage
     format_item = function(item)
-      return string.format("%s %s (%s)", item.id, item.version, item.source)
+      -- take only first line of description
+      local description = item.description:match("^[^\r\n]*")
+      return string.format("%s %s (%s) - %s", item.id, item.version, item.source, description)
     end,
   }, function(package)
-    self.package = package
-    self:_prompt_version()
+    if not package then
+      return
+    end
+    self.selected_package = package
+    self:prompt_version()
   end)
 end
 
-function Nuget:_prompt_version()
-  vim.ui.input({ prompt = "Version: ", default = self.package.version }, function(version)
-    self.version = version
-    self:_prompt_project()
+---@package
+function Nuget:prompt_version()
+  vim.ui.input({ prompt = "Version: ", default = self.selected_package.version }, function(version)
+    self.selected_version = version
+    self:prompt_project()
   end)
 end
 
-function Nuget:_prompt_project()
+---@package
+function Nuget:prompt_project()
   local projects = utils.get_projects(false)
   if #projects == 1 then
-    self.project = projects[1]
-    self:_add_package()
+    self.selected_project = projects[1]
+    self:add_package()
   else
     vim.ui.select(projects, {
       prompt = "Choose project:",
@@ -46,19 +68,21 @@ function Nuget:_prompt_project()
       if not project then
         return
       end
-      self.project = project
-      self:_add_package()
+      self.selected_project = project
+      self:add_package()
     end)
   end
 end
 
-function Nuget:_add_package()
-  local args = { "dotnet", "add", self.project, "package", self.package.id, "--version", self.version }
+---@package
+function Nuget:add_package()
+  local args =
+    { "dotnet", "add", self.selected_project, "package", self.selected_package.id, "--version", self.selected_version }
   vim.system(
     args,
     vim.schedule_wrap(function(output)
       if output.code == 0 then
-        vim.cmd("edit " .. self.project)
+        vim.cmd("edit " .. self.selected_project)
       else
         vim.notify("Error adding package", vim.log.levels.INFO)
       end
@@ -69,7 +93,7 @@ end
 function Nuget:search()
   vim.ui.input({ prompt = "Query: " }, function(input)
     vim.system(
-      { "dotnet", "package", "search", input, "--format", "json", "--take", "20" },
+      { "dotnet", "package", "search", input, "--format", "json", "--take", "20", "--verbosity", "detailed" },
       vim.schedule_wrap(function(output)
         local result = vim.json.decode(output.stdout)
         local packages = {}
@@ -80,11 +104,12 @@ function Nuget:search()
               version = package.latestVersion,
               downloads = package.totalDownloads,
               source = searchResult.sourceName,
+              description = package.description,
             })
           end
         end
         self.packages = packages
-        self:_prompt_package()
+        self:prompt_package()
       end)
     )
   end)
