6f9c1931e29cc16b9b2779e36192f3f83493aece
cantti <cantti@outlook.com>
2025-08-26 11:28:30 +0200

    improve nuget

--- lua/cshelper/nuget.lua
+++ lua/cshelper/nuget.lua
@@ -3,46 +3,13 @@ local fs = require("cshelper.fs")
 
 local M = {}
 
-local function add_package(package, project, version)
-  local args = { "dotnet", "add", project, "package", package.id, "--version", version }
-  local function on_exit(output)
-    if output.code == 0 then
-      vim.cmd("edit " .. project)
-      vim.notify(
-        string.format("Package %s added to project %s", package.id, fs.get_file_name(project)),
-        vim.log.levels.INFO
-      )
-    end
-  end
-  vim.system(args, vim.schedule_wrap(on_exit))
+local H = {}
+
+function M.search()
+  vim.ui.input({ prompt = "Query: " }, H.prompt_package)
 end
 
-local function prompt_version(project, package)
-  vim.ui.input({ prompt = "Version: ", default = package.version }, function(version)
-    add_package(package, project, version)
-  end)
-end
-
-local function prompt_project(package)
-  local projects = utils.get_projects(false)
-  if vim.tbl_count(projects) == 1 then
-    prompt_version(projects[1], package)
-  else
-    vim.ui.select(projects, {
-      prompt = "Choose project:",
-      format_item = function(item)
-        return fs.relative_path(item)
-      end,
-    }, function(project)
-      if not project then
-        return
-      end
-      prompt_version(project, package)
-    end)
-  end
-end
-
-local function prompt_package(input)
+function H.prompt_package(input)
   local function on_exit(output)
     local result = vim.json.decode(output.stdout)
     local packages = {}
@@ -62,21 +29,50 @@ local function prompt_package(input)
     end
     vim.ui.select(packages, {
       format_item = function(item)
-        return string.format(
-          "%-" .. max_id .. "s | Version: %-12s | Downloads: %-10s | Source: %s",
-          item.id,
-          item.version,
-          item.downloads,
-          item.source
-        )
+        return string.format("%s %s (%s)", item.id, item.version, item.source)
       end,
-    }, prompt_project)
+    }, H.prompt_project)
   end
-  vim.system({ "dotnet", "package", "search", input, "--format", "json" }, vim.schedule_wrap(on_exit))
+  vim.system({ "dotnet", "package", "search", input, "--format", "json", "--take", "100" }, vim.schedule_wrap(on_exit))
 end
 
-function M.search()
-  vim.ui.input({ prompt = "Query: " }, prompt_package)
+function H.prompt_project(package)
+  local projects = utils.get_projects(false)
+  if vim.tbl_count(projects) == 1 then
+    H.prompt_version(projects[1], package)
+  else
+    vim.ui.select(projects, {
+      prompt = "Choose project:",
+      format_item = function(item)
+        return fs.relative_path(item)
+      end,
+    }, function(project)
+      if not project then
+        return
+      end
+      H.prompt_version(project, package)
+    end)
+  end
+end
+
+function H.prompt_version(project, package)
+  vim.ui.input({ prompt = "Version: ", default = package.version }, function(version)
+    H.add_package(package, project, version)
+  end)
+end
+
+function H.add_package(package, project, version)
+  local args = { "dotnet", "add", project, "package", package.id, "--version", version }
+  local function on_exit(output)
+    if output.code == 0 then
+      vim.cmd("edit " .. project)
+      vim.notify(
+        string.format("Package %s added to project %s", package.id, fs.get_file_name(project)),
+        vim.log.levels.INFO
+      )
+    end
+  end
+  vim.system(args, vim.schedule_wrap(on_exit))
 end
 
 return M
