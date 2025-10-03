5c757a09df82d6416acf2ff8eed8a179745c774a
cantti <cantti@outlook.com>
2025-10-03 15:55:47 +0200

    commands

--- lua/cshelper/init.lua
+++ lua/cshelper/init.lua
@@ -21,15 +21,111 @@ function H.add_autocommands()
   })
 end
 
+local cs_subcommands = {
+  secrets = {
+    impl = function(args)
+      if vim.tbl_contains(args, "--list") then
+        require("cshelper").secrets_list()
+      else
+        require("cshelper").secrets_edit()
+      end
+    end,
+    complete = function(subcmd_arg_lead)
+      local args = {
+        "--list",
+      }
+      return vim
+        .iter(args)
+        :filter(function(install_arg)
+          return install_arg:find(subcmd_arg_lead) ~= nil
+        end)
+        :totable()
+    end,
+  },
+  ns = {
+    impl = function(args)
+      if vim.tbl_contains(args, "--dir") then
+        require("cshelper").fix_ns_buf()
+      else
+        require("cshelper").fix_ns_dir()
+      end
+    end,
+    complete = function(subcmd_arg_lead)
+      local args = {
+        "--dir",
+      }
+      return vim
+        .iter(args)
+        :filter(function(install_arg)
+          return install_arg:find(subcmd_arg_lead) ~= nil
+        end)
+        :totable()
+    end,
+    -- ...
+  },
+}
+
+---@param opts table
+local function cs_command(opts)
+  local fargs = opts.fargs
+  local subcommand_key = fargs[1]
+  -- Get the subcommand's arguments, if any
+  local args = #fargs > 1 and vim.list_slice(fargs, 2, #fargs) or {}
+  local subcommand = cs_subcommands[subcommand_key]
+  if not subcommand then
+    vim.notify("Rocks: Unknown command: " .. subcommand_key, vim.log.levels.ERROR)
+    return
+  end
+  -- Invoke the subcommand
+  subcommand.impl(args, opts)
+end
+
+-- inspired by https://github.com/lumen-oss/nvim-best-practices
+function H.add_usercommands()
+  vim.api.nvim_create_user_command("Cs", cs_command, {
+    nargs = "+",
+    desc = "My awesome command with subcommand completions",
+    complete = function(arg_lead, cmdline, _)
+      -- Get the subcommand.
+      local subcmd_key, subcmd_arg_lead = cmdline:match("^['<,'>]*Cs[!]*%s(%S+)%s(.*)$")
+      if subcmd_key and subcmd_arg_lead and cs_subcommands[subcmd_key] and cs_subcommands[subcmd_key].complete then
+        -- The subcommand has completions. Return them.
+        return cs_subcommands[subcmd_key].complete(subcmd_arg_lead)
+      end
+      -- Check if cmdline is a subcommand
+      if cmdline:match("^['<,'>]*Cs[!]*%s+%w*$") then
+        -- Filter subcommands that match
+        local subcommand_keys = vim.tbl_keys(cs_subcommands)
+        return vim
+          .iter(subcommand_keys)
+          :filter(function(key)
+            return key:find(arg_lead) ~= nil
+          end)
+          :totable()
+      end
+    end,
+    bang = false,
+  })
+end
+
 ---@class CshelperAutocmdOpts
+---@field enabled boolean
+---@field use_block_ns boolean
+
+---@class CshelperUsercmdOpts
 ---@field enabled boolean
 ---@field use_block_ns boolean
 
 ---@class CshelperOpts
 ---@field autocommands CshelperAutocmdOpts
+---@field usercommands CshelperUsercmdOpts
 local defaults = {
   autocommands = {
-    enabled = false,
+    enabled = true,
+    use_block_ns = false,
+  },
+  usercommands = {
+    enabled = true,
     use_block_ns = false,
   },
 }
@@ -39,6 +135,9 @@ function M.setup(opts)
   M.opts = vim.tbl_deep_extend("force", defaults, opts)
   if M.opts.autocommands.enabled then
     H.add_autocommands()
+  end
+  if M.opts.usercommands.enabled then
+    H.add_usercommands()
   end
 end
 
